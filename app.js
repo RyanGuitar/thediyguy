2a12c14e0aa0da116bf58b5c6fe5479ebcae7268
RyanGuitar <ryanelsguitar@gmail.com>
2023-11-20 17:42:59 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,51 @@
+function initShare() {
+  const shareData = {
+    title: "The DIY Guy",
+    text: "The One You Can Trust",
+    url: "https://shifts-psi.vercel.app",
+  };
+
+  const btn = document.querySelector("#share");
+  btn.addEventListener("click", async () => {
+    try {
+      await navigator.share(shareData);
+    } catch (err) {
+      return;
+    }
+  });
+}
+
+function animateServices() {
+  document.querySelector("#bottom").animate(
+    {
+      transform: ["translate(0, 100%)", "translate(0, 0)"],
+    },
+    {
+      delay: 450,
+      duration: 800,
+      fill: "forwards",
+      easing: "linear",
+    }
+  );
+  initShare();
+}
+
+function animateLogo() {
+  document.querySelector("#logo").animate(
+    {
+      transform: ["scale(0)", "scale(1)"],
+    },
+    {
+      delay: 150,
+      duration: 450,
+      fill: "forwards",
+      easing: "linear",
+    }
+  );
+  animateServices();
+}
+
+window.onload = () => {
+  animateLogo();
+  document.body.oncontextmenu = () => false;
+};
